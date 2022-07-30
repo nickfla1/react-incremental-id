7aaea7440f4980dfe8595ade37e42b417ae328d9
nickfla1 <lanterniniflavio@gmail.com>
2022-07-30 13:51:14 +0200

    add example code

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,33 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { IncrementalProvider, useId } from '../.';
+
+const Test = () => {
+  const titleId = useId();
+  const descriptionId = useId();
+
+  return (
+    <div className="test">
+      <p id={titleId}>Title - <b>{titleId}</b></p>
+      <p id={descriptionId}>Description - <b>{descriptionId}</b></p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <main>
+      <IncrementalProvider>
+        <Test />
+      </IncrementalProvider>
+      <IncrementalProvider prefix="test-">
+        <Test />
+      </IncrementalProvider>
+      <IncrementalProvider initialValue={10} prefix="test-">
+        <Test />
+      </IncrementalProvider>
+    </main>
   );
 };
 
